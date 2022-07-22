91afde53f355d24b55177194e000e2f109b973b0
arvid <arvid.b901@gmail.com>
2022-07-21 22:27:07 -0400

    Learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, rust!");
+    let current_dir = env::current_dir();
+    println!(
+        "Entries modified in the last 24 hours in {:?}:",
+        current_dir
+    );
+    let dire =  current_dir.unwrap();
+    println!("{}", dire.display());
+}
+
+
+fn walk_directory(directory:&str) {
+    // directory.replace("Hello", "replaced");
+    println!("{}", directory);
 }
