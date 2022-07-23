7b402ab06283dbbd0b88981770e06ad8967bdeb5
abushati <arvid.b901@gmail.com>
2022-07-23 18:20:12 -0400

    read directory files in current dir

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::{env, fs};
 
+
+#[derive(Debug)]
+struct File {
+    path: std::path::PathBuf,
+    file_name: std::ffi::OsString,
+    last_accessed: std::time::SystemTime,
+    last_modified:std::time::SystemTime,
+    created: std::time::SystemTime,
+}
+
+
 fn main() {
     let current_dir = env::current_dir();
     println!(
@@ -7,11 +18,33 @@ fn main() {
         current_dir
     );
     let dire =  current_dir.unwrap();
-    println!("{}", dire.display());
+    let files = walk_directory(dire);
+    for file in files {
+        println!("{:?}", file.file_name);
+    }
+    
 }
 
 
-fn walk_directory(directory:&str) {
-    // directory.replace("Hello", "replaced");
-    println!("{}", directory);
+fn walk_directory(directory:std::path::PathBuf) -> Vec<File> {
+    let mut current_dir_files: Vec<File> = Vec::new();
+    let directs = fs::read_dir(directory).unwrap();
+    for  entry in directs {
+        let entry = entry.unwrap();
+        let file_name = entry.file_name();
+        let file_path = entry.path();
+        
+        let metadata = fs::metadata(&file_path).unwrap();
+        if metadata.is_file() {
+            let file = File {
+                path: file_path,
+                file_name: file_name,
+                last_accessed: metadata.accessed().unwrap(),
+                last_modified: metadata.modified().unwrap(),
+                created: metadata.created().unwrap(),
+                };
+            current_dir_files.push(file);
+        }
+    }
+    current_dir_files
 }
