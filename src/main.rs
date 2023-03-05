1fc045d193afdb004bd26687c0430a7a52e76de1
abushati <arvid.b901@gmail.com>
2023-03-04 21:38:27 -0500

    endpoint to read file uploads

--- src/main.rs
+++ src/main.rs
@@ -1,227 +1,36 @@
-
-// use std::{fs,path::{PathBuf, Path},str::FromStr, os};
-// extern crate serde;
-// extern crate serde_json;
-// use serde::{Serialize, Deserialize};
-// use std::io::prelude::*;
-// use std::env::args;
-// use core::fmt::Debug;
-// use std::env;
-
-
-// mod filesystem;
-// use filesystem::{Directory,File, FileManager, file_manager_section};
-// mod filecleaner;
-// use filecleaner::FileCleaner;
-
-// struct CliAction {
-//     cmd: Box<dyn CLICommand>,
-// }
-
-// trait  CLICommand {
-//     fn run(&self){}
-// }
-// enum primary_cmds {
-//     MANAGER,
-//     //  CONFIG,
-//       CLEAN
-//     }
-// impl FromStr for primary_cmds {
-
-//     type Err = ();
-
-//     fn from_str(input: &str) -> Result<primary_cmds, Self::Err> {
-//         let  input = input.to_uppercase();
-//         match input.as_str() {
-//             "MANAGER"  => Ok(primary_cmds::MANAGER),
-//             // "CONFIG"  => Ok(primary_cmds::CONFIG),
-//             "CLEAN"  => Ok(primary_cmds::CLEAN),
-//             _      => Err(()),
-//         }
-//     }
-// }
-
-
-// impl FromStr for file_manager_section {
-
-//     type Err = ();
-
-//     fn from_str(input: &str) -> Result<file_manager_section, Self::Err> {
-//         let  input = input.to_uppercase();
-//         match input.as_str() {
-//             "EXCLUDE_DIR"  => Ok(file_manager_section::EXCLUDE_DIR),
-//             "INCLUDE_DIR"  => Ok(file_manager_section::INCLUDE_DIR),
-//             "EXCLUDE_FILE"  => Ok(file_manager_section::EXCLUDE_FILE),
-//             _      => Err(()),
-//         }
-//     }
-// }
-// #[derive(Debug,PartialEq)]
-// enum manager_actions {
-//     ADD,
-//     REMOVE,
-//     RESET
-//     }
-
-// impl FromStr for manager_actions {
-//     type Err = ();
-//     fn from_str(input: &str) -> Result<manager_actions, Self::Err> {
-//         let  input = input.to_uppercase();
-//         match input.as_str() {
-//             "ADD"  => Ok(manager_actions::ADD),
-//             "REMOVE"  => Ok(manager_actions::REMOVE),
-//             "RESET"  => Ok(manager_actions::RESET),
-//             _      => Err(()),
-//         }
-//     }
-// }  
-
-// struct manager_cmd{
-//     manager_action: manager_actions,
-//     sub_action: Option<file_manager_section>,
-//     value: Option<String>
-// }
-
-// struct filecleaner_cmd{
-//     filemanager: FileManager
-// }
-
-// impl CLICommand for filecleaner_cmd {
-//     fn run(&self) {
-//         let cleaner = FileCleaner{file_manager: self.filemanager.clone(), max_file_age:34, db: None};
-//         cleaner.clean()
-//     }
-// }
-
-// impl CLICommand for manager_cmd {
-//     fn run(&self) {
-//         println!("{:?}",&self.manager_action);
-//         let file_manager = FileManager::default().load();
-//         match self.manager_action {
-//             manager_actions::ADD => {
-//                 file_manager.add(&self.sub_action.as_ref().ok_or("no").unwrap(),self.value.as_ref().ok_or("no").unwrap().as_str());
-//             },
-//             manager_actions::REMOVE => {
-//                 file_manager.remove(&self.sub_action.as_ref().ok_or("no").unwrap(),self.value.as_ref().ok_or("no").unwrap().as_str());
-//             },
-//             manager_actions::RESET => {
-//                 file_manager.reset();
-//             }
-//         }
-//     }
-// }
-
-// fn parse_args() -> Result<CliAction,String> {
-//     let args:Vec<String> = args().collect();
-//     if !(args.len() > 1){
-//         return Err(String::from_str("invalid args len").unwrap());
-//     }
-    
-//     let primary_cmd = args.get(1).unwrap();
-//     match primary_cmds::from_str(&primary_cmd){
-//         Ok(act) => {
-//             match act {
-//                 primary_cmds::MANAGER => {
-//                     let manager_action;
-//                     let manager_section;
-//                     let value;
-//                     if args.get(2).is_none(){
-//                         return Err(format!("No sub action provided, valid args: {:?}" , "adf"));
-//                     }
-//                     match manager_actions::from_str(args.get(2).unwrap()){
-//                         Ok(action) => {
-//                             manager_action = action;
-//                         }
-//                         Err(()) => {
-//                             return Err(format!("Invalid manager action" ));
-//                         }
-//                     }
-                    
-//                     if manager_action == manager_actions::RESET {
-//                         let cmd = manager_cmd{manager_action:manager_action, sub_action: None, value: None};
-//                         return Ok(CliAction{cmd:Box::new(cmd)});
-//                     }
-
-//                     let section = args.get(3).unwrap();
-//                     match file_manager_section::from_str(&section.as_str()) {
-//                         Ok(section) => {
-//                             manager_section = section
-//                         }
-//                         Err(()) => {
-//                             return Err(format!("Invalid subaction {}, valid args: {:?}" ,"adf", "adf"));
-//                         }
-//                     }
-                    
-//                     let path = args.get(4);
-//                     if !path.is_none(){
-//                         let project_root = env::current_exe().unwrap();
-//                         let root:Vec<&str> = project_root.to_str().unwrap().split("jarvis\\target").collect();
-//                         let root_starts_with = root.get(0).unwrap();
-
-//                         let path_buf = PathBuf::from_str(path.unwrap()).unwrap();
-//                         if path_buf.starts_with( root_starts_with){
-//                             return Err("File/directory can't be in project file".to_string());
-//                         }
-                        
-//                         value = path.unwrap();
-
-//                     } else {
-//                         return Err(format!("path not provided"));
-//                     }
-
-//                     let cmd = manager_cmd{manager_action:manager_action,sub_action:Some(manager_section),value:Some(value.to_string())};
-//                     Ok(CliAction{cmd:Box::new(cmd)})
-//                 },
-//                 // primary_cmds::CONFIG => {return Err()},
-//                 primary_cmds::CLEAN => {
-//                     let file_manager = FileManager::default().load();
-//                     let cmd = filecleaner_cmd{filemanager:file_manager};
-//                     Ok(CliAction{cmd:Box::new(cmd)})
-//                 }
-
-//             }
-
-//         }
-//         Err(()) => {return Err(format!("invalid primary arg {}, valid args: {:?}", "primary_cmd", "valid_primary_cmds"));}
-//     } 
-// }
-
-// fn main() {
-//     let db = parse_args();
-//     match db {
-//         Ok(action) => {
-//             let cmd = action.cmd;
-//             cmd.run()
-//             // action.run_action();
-//         },
-//         Err(error) =>{
-//             println!("{}", error)
-//         }
-        
-//     }
-// }
-// src/ftp/client/mod.rs
-
 mod ftpss;
 use ftpss::client::tcpconnect;
 use ftpss::server::server;
+use std::{fs::OpenOptions, io::Write};
+use actix_multipart::Multipart;
 
 
-
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App,HttpRequest, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
+#[get("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+
+#[post("/upload_file")]
+async fn upload_file(request: web::Bytes) -> impl Responder {
+    println!("{:?}",request);
+    let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("foo.docx").unwrap();
+    file.write(&request);
+    HttpResponse::Ok().body("s")
 }
 
 #[actix_web::main]
@@ -232,6 +41,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(upload_file)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
