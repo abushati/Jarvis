4a0a93ba42f1a0e36170906c11520ca7a201f4f2
abushati <arvid.b901@gmail.com>
2023-03-11 19:22:18 -0500

    read from redis success

--- src/bin/read.rs
+++ src/bin/read.rs
@@ -1,18 +1,13 @@
-use bytes::Bytes;
-use std::io::Read;
 use std::{fs::OpenOptions, io::Write};
-use actix_multipart::Multipart;
-use serde::Deserialize;
-use actix_web::{get, post, web, App,HttpRequest, HttpResponse, HttpServer, Responder};
 extern crate redis;
 use redis::Commands;
-use redis::{Value, FromRedisValue,RedisError};
-use std::collections::HashMap;
-use uuid::Uuid;
-use md5;
-use std::str;
 use std::{thread, time::Duration};
-
+#[derive(serde::Deserialize)]
+struct File {
+    fileName: String,
+    saved_md5: String,
+    request: String
+}
 fn main()  {
     loop {
         let client = redis::Client::open("redis://localhost:6379").unwrap();
@@ -20,6 +15,13 @@ fn main()  {
         let key = "upload_queue";
         let data : String = con.lpop(key,None).unwrap();
         println!("{:?}",data);
+        let d = serde_json::from_str::<File>(&data).unwrap();
+        let file_bytes = d.request.as_bytes();
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(d.fileName).unwrap();
+        let _ = file.write_all(file_bytes).unwrap();
         thread::sleep(Duration::from_secs(4));
     }
 }
